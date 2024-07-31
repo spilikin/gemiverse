aff9b6494803e5dd4f845b9035e519e80f2179b6
spilikin <108782+spilikin@users.noreply.github.com>
2024-07-31 21:05:07 +0200

    beta2

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=2.0.0-beta1
+VERSION=2.0.0-beta2
 DOCKER_IMAGE=spilikin/gemiverse:latest
 info:
 
--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,2 +1,2 @@
-export const AppVersion = "2.0.0-alpha1";
-export const BuildDate = "2024-07-31 17:37";
+export const AppVersion = "2.0.0-beta2";
+export const BuildDate = "2024-07-31 21:04";
